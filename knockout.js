0dbb1c2f6191aa58c872210304750629838b0122
Stefan Bols <stefan@imbols.net>
2018-02-06 08:49:51 +0100

    Retrieving songs from spotify api

--- knockout.js
+++ knockout.js
@@ -12,7 +12,13 @@ function Playlist(data) {
 
     self.title = data.title;
     self.songs = data.songs;
+    self.songsCount = data.songsCount;
+    self.trackUrl = data.trackUrl;
     self.selected = ko.observable(data.selected);
+    
+    self.limit = data.limit;
+    self.offset = data.offsetlimit;
+    self.total = data.total;
 
     self.toggleSelected = function() {
         self.selected(!self.selected());
@@ -41,29 +47,75 @@ function SpotifyPlaylistCombinerViewModel() {
             .map('songs')
             .flatten()
             .sortBy('title')
+            .uniq(true, 'title')
             .value()
     });
+    self.loadedPlaylists = ko.observable();
+    self.availablePlaylists = ko.observable();
 
     self.AddSong = function(song) {
         self.songs.push(song);
     }
     self.selectList = function(list) {
-        list.toggleSelected();
+        $.ajax(list.trackUrl + '?fields=total%2Climit%2Coffset%2Cnext%2Cprev%2Citems(track(artists(name)%2C%20name%2C%20id))&limit=100', {
+            dataType: 'json',
+            headers: {
+                'Authorization': 'Bearer BQCnmx-cgeB7s_l16BgnlEGe8-QAomk8OXKT4QFIk1hyaNNqpfKmh16Wc2pZ-vVT1KL7DX6_cfuYEUOBfl2__ER7-Q_JPYCXV-jRLx1At-K_NtLODO2uqe7wbdw2UtZdbtukiHTsJcCd_A5bEJiLYVhHgcG_'
+            },
+            success: function(r) {
+                console.log(r);
+                list.limit = r.limit;
+                list.offset = r.offsetlimit;
+                list.total = r.total;
+                var mappedTasks = _.map(r.items, function(item) {
+                    return new Song({
+                        title: item.track.name,
+                        selected: true
+                    })
+                });
+                list.songs = mappedTasks;
+
+                list.toggleSelected();
+            }
+        });
     }
     self.selectSong = function(song) {
         song.toggleSelected();
     }
 
-    // Get data
-    $.getJSON('api.json', function(allData) {
-        var mappedTasks = $.map(allData, function(item) {
-            return new Playlist({
-                title: item.name,
-                songs: $.map(item.songs, function(song) { return new Song({ title: song, selected: true }) }),
-                selected: false
-            })
-        });
-        self.lists(mappedTasks);
+    // // Get data
+    // $.getJSON('api.json', function(allData) {
+    //     var mappedTasks = $.map(allData, function(item) {
+    //         return new Playlist({
+    //             title: item.name,
+    //             songs: $.map(item.songs, function(song) { return new Song({ title: song, selected: true }) }),
+    //             songsCount: item.songs.length,
+    //             selected: false
+    //         })
+    //     });
+    //     self.lists(mappedTasks);
+    // });
+
+    $.ajax('https://api.spotify.com/v1/me/playlists?limit=50', {
+        dataType: 'json',
+        headers: {
+            'Authorization': 'Bearer BQDxL8tolIEZEzBuEw1AnLVmcY-NjGeZQdHBqa_gBJHdyi100SrohCtpourXo-hv1SdwIQPcBZ4NlP0dgXEhqk6fiHUZnyKQjQRAAXtzwwDDBnD0g8V7D5YKf1mQAy2twowLNfnfvVMoMMlIGP7DXaQvNKB6'
+        },
+        success: function(r) {
+            console.log(r);
+            self.loadedPlaylists(r.items.length);
+            self.availablePlaylists(r.total);
+            var mappedTasks = $.map(r.items, function(item) {
+                return new Playlist({
+                    title: item.name,
+                    //songs: $.map(item.songs, function(song) { return new Song({ title: song, selected: true }) }),
+                    songsCount: item.tracks.total,
+                    selected: false,
+                    trackUrl: item.tracks.href
+                })
+            });
+            self.lists(mappedTasks);
+        }
     });
 }
 
